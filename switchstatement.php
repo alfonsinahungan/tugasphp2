1e5fd80f0fbd5afdff80cf22a8e7f2d875e8f3ad
alfonsinahungan <119381206+alfonsinahungan@users.noreply.github.com>
2023-03-03 10:17:57 +0900

    Add files via upload

---
+++ switchstatement.php
@@ -0,0 +1,21 @@
+<?php
+
+$nilai="E";
+
+switch ($nilai) {
+    case 'A':
+        echo "Rp 15.000" .PHP_EOL;
+        break;
+    case 'B':
+        echo "Rp 25.000" .PHP_EOL;
+        break;
+    case 'C':
+        echo "Rp 35.000" .PHP_EOL;
+        break;
+    case 'D':
+        echo "Rp 45.000".PHP_EOL;
+        break;
+    case 'E':
+        echo "Rp 55.000" . PHP_EOL;
+        break;
+}
---
+++ ternaryoperator.php
@@ -0,0 +1,12 @@
+<?php
+
+$angka = 19;
+$a = $angka % 2;
+
+
+$hi = $a <= 0.00  ?  "bilangan genap"  :   "bilangan ganjil" ;
+
+echo $hi; 
+
+
+
